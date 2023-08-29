8ca4dd5e3c7c397df016f0c699438ee88148265c
Vaishnavi25-hub <vaishnavimd2512@gmail.com>
2023-08-29 08:58:00 +0530

    dummy project started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import ContactList from './ContactList';
+import AddContact from './AddContact';
+import ContactCard from './ContactCard';
+import { v4 as uuid } from "uuid";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {Switch} from 'react-router-dom'
 
 function App() {
+
+  // const contacts=
+  // [
+  //   {"id":1,
+  //     "name":"Vaishnavi",
+  //     "email":"vaishali@gmail.com"
+  //   },
+  //   {"id":2,
+  //     "name":"Vaishali",
+  //     "email":"vaishnavi@gmail.com"
+  //   },
+  //   {"id":3,
+  //     "name":"Sam",
+  //     "email":"sam@gmail.com"
+  //   }
+  // ]  //commenting for creation of hooks
+
+  const [contacts, setContacts] =useState([]);
+ const LOCAL_STORAGE_KEY="contacts";
+
+  //get contact from add contact component define addContactHandler
+
+  const addContactHandler = (contact) =>{
+    console.log(contact)
+    setContacts([...contacts,{id: uuid(), ...contact}])
+
+  }
+
+  const removeContactHandler= (id) =>{
+    const newContactList= contacts.filter((contact) =>
+    {
+      return contact.id !==id;
+    })
+    setContacts(newContactList);
+  }
+    useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='ui container'>
+     
+      <Header/>
+      <Router>
+        <Routes>
+        <Route path="/" exact component={ContactList}></Route>
+          <Route path="/add"component={AddContact}/>
+          
+        </Routes>
+      </Router>
+      {/* <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts} getContactId={removeContactHandler}/> */}
+     
+      
     </div>
+    
+    
   );
 }
 
 export default App;
+
---
+++ src/ContactList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ContactCard from './ContactCard';
+
+const ContactList =(props)=>{
+    console.log(props);
+
+    const deleteContactHandler=(id)=>{
+        props.getContactId(id);
+    }
+
+    const contacts=[
+        {
+            id:"1",
+            "name":"Vaishnavi",
+            "email":"vmd@gmail.com"
+        }
+    ]
+    const renderContactList = props.contacts.map((contact) => {
+        return (
+          <ContactCard
+            contact={contact}
+            clickHandler={deleteContactHandler}
+            key={contact.id}
+          />
+        );
+      });
+      return <div className="ui celled list">{renderContactList}</div>;
+    };
+    
+
+export default ContactList;
